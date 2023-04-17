b0773b342a4bb0d857c165be4e684958526bd5be
CodingSimian <viktor.bernas.ostrand@edu.edugrade.se>
2023-04-17 15:14:57 +0200

    added CRUD functionality for Customer entity, added CustomerServiceImpl file

--- src/main/java/ostrand/wigellcarrental/entities/Customer.java
+++ src/main/java/ostrand/wigellcarrental/entities/Customer.java
@@ -1,6 +1,8 @@
 package ostrand.wigellcarrental.entities;
 
-import jakarta.persistence.*;
+//import jakarta.persistence.*;
+
+import javax.persistence.*;
 
 @Entity
 @Table(name = "customers")
@@ -8,5 +10,58 @@ public class Customer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
     private Long id;
+
+    @Column(length = 40)
+    private String username;
+
+    @Column(length = 60)
+    private String name;
+
+    @Column(length = 70)
+    private String email;
+
+    @Column(length = 14)
+    private Integer phone_nmbr;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getPhone_nmbr() {
+        return phone_nmbr;
+    }
+
+    public void setPhone_nmbr(Integer phone_nmbr) {
+        this.phone_nmbr = phone_nmbr;
+    }
 }
---
+++ src/main/java/ostrand/wigellcarrental/services/CustomerServiceImpl.java
@@ -0,0 +1,46 @@
+package ostrand.wigellcarrental.services;
+
+import org.springframework.stereotype.Service;
+import ostrand.wigellcarrental.entities.Customer;
+import ostrand.wigellcarrental.repositories.CustomerRepository;
+
+import java.util.List;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+    private final CustomerRepository customerRepository;
+
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+    customerRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Customer> listCustomers() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public void updateCustomerById(Long id, Customer customerToBeUpdated) {
+    Customer exisisting = customerRepository.findById(id).get();
+
+        exisisting.setEmail(customerToBeUpdated.getEmail());
+        exisisting.setName(customerToBeUpdated.getName());
+        exisisting.setUsername(customerToBeUpdated.getUsername());
+        exisisting.setPhone_nmbr(customerToBeUpdated.getPhone_nmbr());
+
+        customerRepository.save(exisisting); //save ärvs av Crudrepository interfacet, vilket då innebär att
+        //om objektets id inte finns i repositoriet så skapas ett nytt objekt, och om det redan finns så uppdateras
+        //bara det redan existerande objektet
+    }
+
+    @Override
+    public Customer addCustomer(Customer customerToBeAdded) {
+
+        return customerRepository.save(customerToBeAdded);
+    }
+}
